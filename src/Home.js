60453fe7419d578a6ebb4537d368bc6abe628bc8
Fajar Windhu Zulfikar <fajarwindhuzulfikar@gmail.com>
2022-09-15 22:08:00 +0700

    done

---
+++ src/Home.js
@@ -0,0 +1,78 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { GoogleLogin } from '@react-oauth/google';
+
+export default function Home() {
+  const [jsonResponse, setJsonResponse] = useState();
+  const [apiHost, setApiHost] = useState("http://127.0.0.1:8000");
+  const [isError, setIsError] = useState(false);
+
+  const wrapperCodeStyle = {
+    width: "100%"
+  }
+  const codeStyle = {
+    fontSize: 12,
+    wordWrap: "anywhere"
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Noobium Sign In With Google Test</p>
+
+        <div>
+          <form>
+            <label><small>Set API Host</small></label>
+            <input value={apiHost} onChange={(e) => setApiHost(e.target.value)} />
+          </form>
+        </div>
+
+        <br />
+
+        <div>API Endpoint: {apiHost}/api/sign-in/google</div>
+
+        <br />
+
+        <GoogleLogin
+          onSuccess={response => {
+            setJsonResponse()
+            fetch(`${apiHost}/api/sign-in/google`, {
+              headers: new Headers({ accept: "application/json" }),
+              method: 'POST',
+              body: JSON.stringify({token: response.credential}),
+            })
+              .then((response) => {
+                if (response.ok) {
+                  return response.json();
+                }
+                throw new Error("Something went wrong!");
+              })
+              .then((data) => setJsonResponse(data))
+              .catch((error) => {
+                setIsError(true)
+                console.log(error.TypeError);
+              });
+          }}
+          onError={() => {
+            setJsonResponse()
+            console.log('Login Failed');
+          }}
+        />
+        <div>
+          { 
+            jsonResponse &&
+            <div style={wrapperCodeStyle}>
+              <p>Login success! Here is your info</p>
+              <code style={codeStyle}>
+                  { JSON.stringify(jsonResponse, null, 2) }
+              </code>
+            </div>
+          }
+          { isError && <div>Error occured, See Console!</div> }
+        </div>
+      </header>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId="1086891902310-jinqndhi2522cd81b82gtaro7vevgjed.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
