b497562326f6d219cf47b8415032e161f22dc558
Fajar Windhu Zulfikar <fajarwindhuzulfikar@gmail.com>
2022-09-15 22:12:00 +0700

    chore: remove dashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/sign-in/google" element={<Dashboard />} />
       </Routes>
     </>
   );
